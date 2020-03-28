a6c7f5cbdfbc843918e82b1310c51bf07c38f169
Marcelo Reis <marcelo.ag.reis@gmail.com>
2020-03-28 19:34:32 -0300

    Cria botoes e atualiza pacotes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import Home from "./pages/home";
 import Stock from "./pages/stock";
@@ -14,6 +15,7 @@ import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
+    <CssBaseline />
     <NavBar />
     <Switch>
       <Route exact path="/" component={Home} />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 import logo from "../logo.svg";
 import "../App.css";
 
-import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
+const useStyles = makeStyles({
+  buttonContainer: {
+    width: "688px",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    width: "160px",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,27 +37,48 @@ const Home = () => {
           Seja Bem-Vindo ao protótipo do que algum dia sera o estoque do São
           Chico.
         </p>
-        <Link to="estoque">
-          <Button size="small" variant="contained" color="primary">
+
+        <div className={classes.buttonContainer}>
+          <Button
+            className={classes.button}
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => history.push("estoque")}
+          >
             Estoque
           </Button>
-        </Link>
 
-        <Link to="adicionar">
-          <Button size="small" variant="contained" color="primary">
+          <Button
+            className={classes.button}
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => history.push("adicionar")}
+          >
             Adicionar
           </Button>
-        </Link>
 
-        <Link to="remover">
-          <Button size="small" variant="contained" color="primary">
+          <Button
+            className={classes.button}
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => history.push("remover")}
+          >
             Retirar
           </Button>
-        </Link>
 
-        <Button size="small" variant="contained" color="primary">
-          Compras
-        </Button>
+          <Button
+            className={classes.button}
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => history.push("compras")}
+          >
+            Compras
+          </Button>
+        </div>
       </header>
     </div>
   );
