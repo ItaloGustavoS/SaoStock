1c8fd0b6e013d68d0f4fe221bc8be68e5b4595a2
Ítalo <42496107+ItaloGustavoS@users.noreply.github.com>
2020-04-11 17:12:20 -0300

    chore: Nova build

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -113,7 +113,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/login" variant="body2">
                 Já tem uma conta? Entrar
               </Link>
             </Grid>
